258bc80756e9be6fa24cd81e81d4728a41390c2f
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-13 15:32:13 +0900

    React & JSX 1.7
    JSXでトップページの構成要素を記述する

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,16 @@
-/*export default function Home() {
-  const test = (
-    <div>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-}*/
-
-/*import React from "react";
 export default function Home() {
-  return (
-    <React.Fragment>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </React.Fragment>
-  );
-}*/
-
-/*export default function Home() {
   return (
     <>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <footer>FOOTER</footer>
     </>
-  );
-}*/
-/*export default function Home() {
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <hr />
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}*/
-/*export default function Home() {
-  return (
-    <div className="hero">
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}*/
-/*export default function Home() {
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}*/
-/*export default function Home() {
-  return (
-    <div>
-      <h1 style={{ color: 'blue', fontSize: '80px'}}>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}*/
-export default function Home() {
-  const subtitle = 'アウトプットしていくサイト'
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <p>{subtitle}</p>
-    </div>
   )
 }
