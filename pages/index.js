7faadc267cbfa2345ed98b6c95bad2e45505aeb3
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-13 16:06:19 +0900

    Components 2.3
    コンポーネントを作る

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,16 @@
-function EachPost(props) {
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
+export default function Home() {
   return (
-    <article>
-      <a href={props.url}>
-        <h3>{props.url}</h3>
-      </a>
-    </article>
-  )
-}
-/*
- function EachPost({ title, url }) {
-  return (
-    <article>
-      <a href={url}>
-        <h3>{title}</h3>
-      </a>
-    </article>
-  )
-}*/
-/*export default function Posts() {
-  return (
-    <section>
-      <h2>おすすめ記事</h2>
-      <article>
-        <a href="/blog/schedule">
-          <h3>スケジュール管理と猫の理論</h3>
-        </a>
-      </article>
-      <article>
-        <a href="/blog/music">
-          <h3>音楽が呼び起こす美味しいものの記憶</h3>
-        </a>
-      </article>
-    </section>
-  );
-}*/
-/*
-export default function Posts() {
-  const props1 = { title: '記事のタイトル1', url: 'post1.html' }
-  const props2 = { title: '記事のタイトル2', url: 'post2.html' }
+    <>
+      <Header />
 
-  return (
-   <section>
-    <h2>おすすめ記事</h2>
-    <EachPost {...props1} />
-    <EachPost {...props2} />
-  )
-} */
+      <main>
+        <Hero />
+      </main>
 
-function Decoration(props) {
-  return (
-    <div style={{ color: 'red' }}>
-      {props.children}
-    </div>
-  )
-}
-/*
-function Decoration({ children }}) {
-  return (
-    <div style={{ color: 'red' }}>
-      {children}
-    </div>
-  )
-}*/
-
-export default function Hero() {
-  return (
-    <Decoration>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </Decoration>
+      <Footer />
+    </>
   )
 }
