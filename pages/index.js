e56ed0995f0b4ce39dc73fda3461637e05c217e6
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-13 15:29:05 +0900

    React ＆JSX 1.6
    JSXのルールを確認する

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,68 @@
+/*export default function Home() {
+  const test = (
+    <div>
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </div>
+}*/
+
+/*import React from "react";
 export default function Home() {
-  return <h1>CUBE</h1>
+  return (
+    <React.Fragment>
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </React.Fragment>
+  );
+}*/
+
+/*export default function Home() {
+  return (
+    <>
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </>
+  );
+}*/
+/*export default function Home() {
+  return (
+    <div>
+      <h1>CUBE</h1>
+      <hr />
+      <p>アウトプットしていくサイト</p>
+    </div>
+  )
+}*/
+/*export default function Home() {
+  return (
+    <div className="hero">
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </div>
+  )
+}*/
+/*export default function Home() {
+  return (
+    <div>
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </div>
+  )
+}*/
+/*export default function Home() {
+  return (
+    <div>
+      <h1 style={{ color: 'blue', fontSize: '80px'}}>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </div>
+  )
+}*/
+export default function Home() {
+  const subtitle = 'アウトプットしていくサイト'
+  return (
+    <div>
+      <h1>CUBE</h1>
+      <p>{subtitle}</p>
+    </div>
+  )
 }
