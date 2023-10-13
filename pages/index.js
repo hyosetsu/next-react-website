2b1aad9259c75811ac9a7cde5e9b737bbe26ecb0
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-13 16:23:24 +0900

    Components 2.6
    カスタムAppコンポーネントでLayoutを全ページに入れる

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
-import Layout from 'components/layout'
 import Hero from 'components/hero'
 export default function Home() {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  )
+  return <Hero />
 }
