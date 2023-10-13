ad64ceac9cbef02b0990049660d1cdbe306cd653
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-13 15:57:48 +0900

    Components 2.2
    コンポーネントのprops

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,73 @@
-function EachPost() {
+function EachPost(props) {
   return (
     <article>
-      <a href="post.html">
-        <h3>記事のタイトル</h3>
+      <a href={props.url}>
+        <h3>{props.url}</h3>
       </a>
     </article>
   )
 }
-
-export default function Posts() {
+/*
+ function EachPost({ title, url }) {
+  return (
+    <article>
+      <a href={url}>
+        <h3>{title}</h3>
+      </a>
+    </article>
+  )
+}*/
+/*export default function Posts() {
   return (
     <section>
       <h2>おすすめ記事</h2>
-      <EachPost />
-      <EachPost />
+      <article>
+        <a href="/blog/schedule">
+          <h3>スケジュール管理と猫の理論</h3>
+        </a>
+      </article>
+      <article>
+        <a href="/blog/music">
+          <h3>音楽が呼び起こす美味しいものの記憶</h3>
+        </a>
+      </article>
     </section>
+  );
+}*/
+/*
+export default function Posts() {
+  const props1 = { title: '記事のタイトル1', url: 'post1.html' }
+  const props2 = { title: '記事のタイトル2', url: 'post2.html' }
+
+  return (
+   <section>
+    <h2>おすすめ記事</h2>
+    <EachPost {...props1} />
+    <EachPost {...props2} />
+  )
+} */
+
+function Decoration(props) {
+  return (
+    <div style={{ color: 'red' }}>
+      {props.children}
+    </div>
+  )
+}
+/*
+function Decoration({ children }}) {
+  return (
+    <div style={{ color: 'red' }}>
+      {children}
+    </div>
+  )
+}*/
+
+export default function Hero() {
+  return (
+    <Decoration>
+      <h1>CUBE</h1>
+      <p>アウトプットしていくサイト</p>
+    </Decoration>
   )
 }
