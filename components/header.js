f7ff3d310ab1337464deb724e7965dade71dc219
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-10-20 14:20:39 +0900

    Components 2.10
    訂正

--- components/header.js
+++ components/header.js
@@ -1,10 +1,10 @@
-import Logo from "./logo";
-import Nav from "components/nav";
+import Logo from "./logo"
+import Nav from "components/nav"
 export default function Header() {
   return (
     <header>
       <Logo />
       <Nav />
     </header>
-  );
+  )
 }
