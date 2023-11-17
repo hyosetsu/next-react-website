c3916818a70ff8491361d079995a540576c6e3a4
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-11-17 14:11:47 +0900

    CSS Modules 3.5
    ヒーローのテキストのスタイルを設定する

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,13 @@
+import styles from './styles/hero.module.css'
+
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
       <div>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
+        <div className="style.text">
+          <h1 className='style.title'>{title}</h1>
+          <p className='style.subtitle'>{subtitle}</p>
+        </div>
         {imageOn && <figure>[画像]</figure>}
       </div>
-    )
+    );
 }
