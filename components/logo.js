5a6708252b9c2d025ae07c3afdef9ff04e836926
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-11-17 16:30:15 +0900

    訂正

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from 'styles/logo.module.css'
+import styles from './styles/logo.module.css'
 export default function Logo({ boxOn = false }) {
     return (
         <Link href="/">
