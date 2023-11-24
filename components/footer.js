2bc271f5eb92ac34d5cfe32242494f8848d652d8
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-11-24 13:40:13 +0900

    訂正

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 import Container from 'components/container';
 import Logo from './logo'
-import styles from 'styles/footer.module.css'
+import styles from '../styles/footer.module.css'
 export default function Footer() {
     return (
       <footer className={styles.wrapper}>
