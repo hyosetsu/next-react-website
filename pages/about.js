48ebc3fb9a49fd005bf46bf75b32de65fc5e2ad5
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-11-24 13:57:38 +0900

    Layout Styles 4.5
    Containerコンポーネントで横幅を整える

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Hero from "components/hero"
 export default function About() {
   return (
     <Container>
-      <Hero title="CUBE" subtitle="アウトプットしていくサイト" />
+      <Hero title="CUBE" subtitle="About development activities" />
       <p>
         Cubeが得意とする分野はモノ作りです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
       </p>
