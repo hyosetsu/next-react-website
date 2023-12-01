5b38d2d2eadbba0ee26ec0ec645150420c520d3f
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-12-01 15:39:36 +0900

    Metadata 6.4
    サイトに関する情報を共有できる形で用意する

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,15 @@
 import Head from "next/head"
 
+// サイトに関する情報
+import { sitaMeta } from "lib/contants"
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = sitaMeta
 export default function Meta({ pageTitle }) {
+    // ページのタイトル
+    const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
     return (
         <Head>
-            <title>{pageTitle}</title>
-            <meta property="og:title" content={pageTitle} />
+            <title>{pageTitle} | {siteTitle}</title>
+            <meta property="og:title" content={'${pageTitle} | ${siteTitle}'} />
         </Head>
     )
 }
