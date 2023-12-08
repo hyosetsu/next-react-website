5f508f69a427f1d7452b5e3cf59a58986247c190
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-12-08 14:02:40 +0900

    訂正

--- components/social.js
+++ components/social.js
@@ -12,7 +12,7 @@ export default function Social({ iconSize = 'initial' }) {
       <ul className={styles.list} style={{ '--icon-size': iconSize }}>
         <li href="https://twitter.com/">
             <FontAwesomeIcon icon={faTwitter} />
-            <span className="sr-only">Twitter</span>
+            <span className='sr-only'>Twitter</span>
         </li>
         <li href="https://www.facebook.com/">
             <FontAwesomeIcon icon={faFacebookF} />
