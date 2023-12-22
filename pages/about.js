b3239b722c4df0afa305b4eee4cbc002ac04da0d
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2023-12-22 14:29:36 +0900

    Chapter9.1 Dynamic Routes
    アイキャッチ画像eyecatchの代替画像とブラー画像を用意する

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,15 @@
-import Meta from 'components/meta'
-import Container from "components/container"
-import Hero from "components/hero"
-import PostBody from "components/post-body"
-import Contact from "components/contact"
-import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column"
-import Image from 'next/image'
-import eyecatch from '../images/about.jpg'
+import Meta from "components/meta";
+import Container from "components/container";
+import Hero from "components/hero";
+import PostBody from "components/post-body";
+import Contact from "components/contact";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "components/two-column";
+import Image from "next/legacy/image";
+import eyecatch from "../images/about.jpg";
 export default function About() {
   return (
     <Container>
--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,13 +1,21 @@
-import { getPostBySlug } from "lib/api"
-import { extractText } from "lib/extract-text"
-import Meta from "components/meta"
-import Container from "components/container"
-import PostHeader from "components/post-header"
-import PostBody from "components/post-body"
-import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column"
-import ConvertBody from "components/convert-body"
-import PostCategories from "components/post-categories"
-import Image from 'next/image'
+import { getPostBySlug } from "lib/api";
+import { extractText } from "lib/extract-text";
+import Meta from "components/meta";
+import Container from "components/container";
+import PostHeader from "components/post-header";
+import PostBody from "components/post-body";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "components/two-column";
+import ConvertBody from "components/convert-body";
+import PostCategories from "components/post-categories";
+import Image from "next/legacy/image";
+import { getPlaiceholder } from "plaiceholder";
+
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants";
 
 export default function Schedule({
   title,
@@ -26,8 +34,8 @@ export default function Schedule({
         pageImgH={eyecatch.height}
       />
       <article>
-        <PostHeader title={title} subtitle="Blog Article" publish={publish} /> 
-        
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
         <figure>
           <Image
             src={eyecatch.url}
@@ -37,6 +45,8 @@ export default function Schedule({
             height={eyecatch.height}
             sizes="(min-width: 1152px) 1152px, 100vm"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -52,24 +62,27 @@ export default function Schedule({
         </TwoColumn>
       </article>
     </Container>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const slug = "schedule"
+  const slug = "schedule";
 
-  const post = await getPostBySlug(slug)
-  const description = extractText(post.content)
-  console.log('~~~~~post~~~~~')
-  console.log(post)
+  const post = await getPostBySlug(slug);
+  const description = extractText(post.content);
+  const eyecatch = post.eyecatch ?? eyecatchLocal;
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
+  console.log("~~~~~post~~~~~");
+  console.log(post);
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
     },
-  }
+  };
 }
