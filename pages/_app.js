679f471964bf26cef3a71466f6199cdd41459f56
hyosetsu <131899359+hyosetsu@users.noreply.github.com>
2024-02-02 16:30:03 +0900

    訂正

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,14 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import 'styles/globals.css'
-import Layout from "components/layout"
-import Script from 'next/script'
-import * as gtag from '../lib/gtag'
+import "styles/globals.css";
+import Layout from "components/layout";
 
 //Font Awesomeの設定
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import { config } from '@fortawesome/fontawesome-svg-core'
-config.autoAddCss = false
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  // const router = useRouter()
-  // useEffect(() => {
-  //   const handleRouteChange = (url) => {
-  //     gtag.pageview(url)
-  //   }
-  //   router.events.on('routeChangeComplete', handleRouteChange)
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange)
-  //   }
-  // },[router.events])
   return (
     <>
-      {/* <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            if ( !typeof window === 'undefined' ) {              
-              window.dataLayer = window?.dataLayer || [];
-              function gtag(){datalayer.push(arguments);}
-              gtag('js', new Date());
-          
-              gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-            }
-          `,
-        }}
-      /> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
@@ -49,4 +16,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default MyApp;
